81eab1aa0ee93e07825cbc522cc0468225a17f3a
Galina Pitts <galina_pitts@icloud.com>
2019-01-29 22:29:30 -0800

    first

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="" /> Built by Galina Pitts
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
